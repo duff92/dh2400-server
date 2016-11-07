eb7e8edd953c3f7e2dc565d04d622c9dcc92204a
duff92 <joakimlarsson92@gmail.com>
2016-11-07 23:18:31 +0100

    Added the data sent from Arduino to live stream in the browser

--- public/app.js
+++ public/app.js
@@ -3,7 +3,11 @@
  */
 $(function(){
     var socket = io();
+    var $player1h1 = $('.player1').find('h1');
+    var $player2h1 = $('.player2').find('h1');
+
     socket.on('message', function (data) {
-        console.log(data);
+        var data = data.msg.split('AT+CIPSEND=0,19')[1];
+        $player1h1.text(data.toString());
     });
 });
